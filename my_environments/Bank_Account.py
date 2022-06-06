8382b48a7ab58601396a47a70ad99b2ad88ba36e
Roaa Albishri <roaa20101@hotmail.com>
2022-06-06 22:38:44 +0300

    modified

--- my_environments/Bank_Account.py
+++ my_environments/Bank_Account.py
@@ -10,8 +10,6 @@ class BankAccount:
 		# your code here (increases the account balance by the given amount)
         self.account_balance += amount
         return self
-
-
     def withdraw(self, amount):
 		# your code here
         if self.account_balance - amount > 0:
@@ -20,22 +18,21 @@ class BankAccount:
             print("Insufficient funds: Charging a $5 fee")
             self.account_balance -= 5
         return self
-
     def display_account_info(self):
 		# your code here
         print("Balance: $" ,self.account_balance)
         return self
-
-
     def yield_interest(self):
         # your code here
         if self.account_balance > 0:
             self.account_balance += (self.account_balance * self.rate)
         else:
-            print("your balance is negative!")
+            print("The balance of your bank account is negative!")
         return self
-
 Roaa= BankAccount()
 Fidaa = BankAccount()
-Roaa.deposit(3).withdraw(1).yield_interest().display_account_info()
-Roaa.deposit(2).withdraw(4).yield_interest().display_account_info()
+print(" ROAA --- BankAcount Information:")
+Roaa.deposit(3).withdraw(1).display_account_info().yield_interest()
+# print("")
+print(" FIDAA -- BankAcount Information:")
+Fidaa.deposit(2).withdraw(4).display_account_info().yield_interest()
