b82f469e33f8362d7b21305b29b14ecbd6d5ee0c
Roaa Albishri <roaa20101@hotmail.com>
2022-06-06 22:13:13 +0300

    initial account

---
+++ my_environments/Bank_Account.py
@@ -0,0 +1,41 @@
+class BankAccount:
+    def __init__(self, int_rate=0, balance=0): 
+        #         don't forget to add some default values for these parameters!
+        # 		your code here! (remember, this is where we specify the attributes for our class)
+        self.rate=int_rate
+        self.account_balance=balance
+        # don't worry about user info here; we'll involve the User class soon
+        
+    def deposit(self, amount):
+		# your code here (increases the account balance by the given amount)
+        self.account_balance += amount
+        return self
+
+
+    def withdraw(self, amount):
+		# your code here
+        if self.account_balance - amount > 0:
+            self.account_balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.account_balance -= 5
+        return self
+
+    def display_account_info(self):
+		# your code here
+        print("Balance: $" ,self.account_balance)
+        return self
+
+
+    def yield_interest(self):
+        # your code here
+        if self.account_balance > 0:
+            self.account_balance += (self.account_balance * self.rate)
+        else:
+            print("your balance is negative!")
+        return self
+
+Roaa= BankAccount()
+Fidaa = BankAccount()
+Roaa.deposit(3).withdraw(1).yield_interest().display_account_info()
+Roaa.deposit(2).withdraw(4).yield_interest().display_account_info()
