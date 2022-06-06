1e5d126ae28dc75a211c8bca29df36bf0c923259
Roaa Albishri <roaa20101@hotmail.com>
2022-06-06 10:00:25 +0300

    initial commit

--- my_environments/Functions_Intermediate_I.py
+++ my_environments/Functions_Intermediate_I.py
@@ -8,18 +8,17 @@ import random
 # 
 print("---------1-------------")
 def randInt(min = 0,max = 100):
-    if max<0:
-        print("number isn't  greater than 0, Try again!")
-    elif min > max:
-        print(" max number isn't greater than the min number , Try again!")
-    else: 
+    if max>0 and min<max: 
         if max != 100 and min != 0:
             return round(random.random()*(max-min)+min)
         if min != 0:
             return round(random.random()*(100-min)+min)
         if max != 100:
             return round(random.random()*max)
+    else:
+        print("Try again, The num should be min > max,and  max < 0")
     return round(random.random()*100)
+
 print(randInt()) # should print a random integer between 0 to 100
 print(randInt(max=50)) 	# should print a random integer between 0 to 50
 print(randInt(min=50)) 	# should print a random integer between 50 to 100
