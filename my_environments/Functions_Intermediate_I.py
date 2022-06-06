82c6f0ed7bad8ead194ea1b269a6b6fdb4fd080d
Roaa Albishri <roaa20101@hotmail.com>
2022-06-06 09:27:49 +0300

    initial commit

---
+++ my_environments/Functions_Intermediate_I.py
@@ -0,0 +1,28 @@
+
+import random
+
+# If no arguments are provided, the function should return a random integer between 0 and 100.
+# If only a max number is provided, the function should return a random integer between 0 and the max number.
+# If only a min number is provided, the function should return a random integer between the min number and 100
+# If both a min and max number are provided, the function should return a random integer between those 2 values.
+# 
+print("---------1-------------")
+def randInt(min = 0,max = 100):
+    if max<0:
+        print("sorry we need a max number greater than 0")
+    elif min > max:
+        print("sorry please give a max greater than the min")
+    else: 
+        if max != 100 and min != 0:
+            return round(random.random()*(max-min)+min)
+        if min != 0:
+            return round(random.random()*(100-min)+min)
+        if max != 100:
+            return round(random.random()*max)
+    return round(random.random()*100)
+print(randInt()) # should print a random integer between 0 to 100
+print(randInt(max=50)) 	# should print a random integer between 0 to 50
+print(randInt(min=50)) 	# should print a random integer between 50 to 100
+print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+
+
