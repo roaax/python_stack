8896d82b35697819f2d0c9319226dbd03da6e22a
Roaa Albishri <roaa20101@hotmail.com>
2022-06-08 10:52:26 +0300

    initial commit

--- my_environments/Zoo.py
+++ my_environments/Zoo.py
@@ -1,18 +1,60 @@
+from re import L
+
+
 class Zoo:
-    def __init__(self, zoo_name):
+    animals=[]
+    def __init__(self, zoo_name , age , hp, hl):
         self.animals = []
         self.name = zoo_name
+        self.age = 0
+        self.hp = hl
+        self.hl= hp
+
     def add_lion(self, name):
         self.animals.append( Lion(name) )
+
     def add_tiger(self, name):
         self.animals.append( Tiger(name) )
+
     def print_all_info(self):
         print("-"*30, self.name, "-"*30)
         for animal in self.animals:
             animal.display_info()
-zoo1 = Zoo("John's Zoo")
-zoo1.add_lion("Nala")
-zoo1.add_lion("Simba")
-zoo1.add_tiger("Rajah")
-zoo1.add_tiger("Shere Khan")
-zoo1.print_all_info()
+
+    def display_info(self):
+        print(f"Name: {self.name} | Health Level: {self.hp} | Happieness Level: {self.hl} Feed: { feed(self.amount)}")
+    
+    def feed(self,amount):
+        for x in range(amount):
+            self.hp = self.hp+10
+            self.hl = self.hl+10
+        return self
+
+
+class Lion(Zoo):
+    def __init__(self, zoo_name,speed=100):
+        super().__init__(zoo_name, age=20 , hp=300 , hl=95)
+        self.speed=speed
+        
+class Tiger(Zoo):
+    def __init__(self, zoo_name,):
+        super().__init__(zoo_name, age=14 , hp=350 , hl=100)
+        
+        
+class Bear(Zoo):
+    def __init__(self, zoo_name,):
+        super().__init__(zoo_name, age=10 , hp=150 , hl=80)
+        
+
+lion = Lion("Lions")
+lion.add_lion("Nala")
+lion.add_lion("Simba")
+tiger= Tiger("Tigers")
+tiger.add_tiger("Rajah")
+tiger.add_tiger("Shere Khan")
+print(lion.feed(40))
+bear= Tiger("Tigers")
+tiger.add_tiger("Rajah")
+tiger.add_tiger("Shere Khan")
+lion.print_all_info()
+tiger.print_all_info()
