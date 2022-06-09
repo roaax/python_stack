03c7cc7f1712489ef1305c74baa368d2111bcfc7
Roaa Albishri <roaa20101@hotmail.com>
2022-06-09 13:27:56 +0300

    new

---
+++ Flask/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,7 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True) 
