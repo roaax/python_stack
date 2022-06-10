84eaee098ae0df8a335d4241e5a5263a0885dac1
Roaa Albishri <roaa20101@hotmail.com>
2022-06-10 15:59:25 +0300

    initial commit

--- my_environments/Zoo/animals.py
+++ my_environments/Zoo/animals.py
@@ -18,7 +18,7 @@ class Cheetah (Animal):
     def __init__(self, name):
         super().__init__(name, age=15, hl=90, hp=90)
         self.type="Cheetah"
-        self.unique= "speed is: 130km/h"
+        self.unique= "Features : speed is 130km/h"
 
     def feed(self):
         super().feed(health=91, happiness=95)
@@ -27,7 +27,7 @@ class Deer(Animal):
     def __init__(self, name):
         super().__init__(name, age=20 , hl=50, hp=60)
         self.type="Deer"
-        self.unique = "Antlers"
+        self.unique = "Features : Antlers"
 
     def feed(self):
         super().feed(health=60, happiness=30)
@@ -36,7 +36,7 @@ class Crocodile(Animal):
     def __init__(self, name):
         super().__init__(name, age=25 , hl=80, hp=60)
         self.type="Crocodile"
-        self.unique= "Skin" 
+        self.unique= "Features : Skin" 
 
     def feed(self):
         super().feed(health=80, happiness=700)
