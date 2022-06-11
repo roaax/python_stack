d5dae2fa4e40b17fa88369f19d6518f77eeb5c1b
Roaa Albishri <roaa20101@hotmail.com>
2022-06-11 21:09:43 +0300

    initial commit

--- django2/firstDjango/firstDjangoApp/views.py
+++ django2/firstDjango/firstDjangoApp/views.py
@@ -8,10 +8,10 @@ def new(request):
     return HttpResponse(" placeholder to display a new form to create a new blog")
 def creat(request):
     return redirect("/")
-def show(request):
-    return HttpResponse("placeholder to display blog number: {number}")
-def edit(request):
-    return HttpResponse("placeholder to edit blog {number}")
-def destroy(request):
+def show(request , number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+def edit(request , number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+def destroy(request , number):
     return redirect("/blogs")
 
