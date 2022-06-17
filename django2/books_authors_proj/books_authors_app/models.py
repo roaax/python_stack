52f767056d5c5f21973519f6f90a1a3ef952c8b6
Roaa Albishri <roaa20101@hotmail.com>
2022-06-17 16:43:19 +0300

    initial commit

---
+++ django2/books_authors_proj/books_authors_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name="authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
