73b54e4bb492cfb76f621b3a02416ee2032c0a93
Roaa Albishri <roaa20101@hotmail.com>
2022-06-19 21:48:01 +0300

    initial commit

--- django2/TVShows_proj/TVShows_app/urls.py
+++ django2/TVShows_proj/TVShows_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("shows/new",views.addShow),
     path("shows/create",views.createShow),
     path("shows/<int:show_id>", views.show_info),
-    path("shows/<int:show_id>/delete", views.deleteShow),
+    path("shows/<int:show_id>/destroy", views.deleteShow),
     path("shows", views.allShows),
     path("shows/<int:show_id>/edit", views.editShow),
     path("shows/<int:show_id>/update", views.updateShow),
